803f56acc8ce0abd746af95c273a292709a3afb1
Achal Thakare <achalthakare123@gmail.com>
2025-09-19 04:23:20 -0700

    update

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve static files from client/dist (Vite build output)
   app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
   });
 }
